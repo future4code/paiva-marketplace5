70f861904cc95ef66444ce71194f7f40a328c634
Carlos-hr <carlos.henrique17.chr@gmail.com>
2021-05-26 20:59:14 -0300

    correção de conflito e do import filtro

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react'
-
-import { AppContainer } from './components/AppContainer'
-import {FiltroOrdernar} from './components/FiltroOrdenar/FiltroOrdenar'
+// import { AppContainer } from './components/AppContainer'
+import FiltroOrdenar from './components/FiltroOrdenar/FiltroOrdenar'
 import Header from './components/Header';
 
 
 class App extends React.Component {
 	render(){
 	return (
-
-        <AppContainer />
-		<FiltroOrdernar/>
+ 	<div>
+        {/* <AppContainer /> */}
+		<FiltroOrdenar/>
         <Header />
-		
+	</div>	
 
 	)
 }
